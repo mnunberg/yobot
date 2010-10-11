fb0ab6ad750fd9e57586ae3d30db54edbb1e4d8b
Mark Nunberg <mnunberg@haskalah.org>
2010-10-11 00:04:15 -0700

    1. Added a "Recent" mechanism for account input classes. 2. Added extra formatting option for backlog/delayed text 3. Made the ignore boxes work

--- py/gui/qyobot.py
+++ py/gui/qyobot.py
@@ -42,10 +42,11 @@ from PyQt4.QtGui import (QComboBox, QMainWindow, QStandardItemModel, QStandardIt
                          QListWidget, QListWidgetItem, QStyledItemDelegate,
                          QStyleOptionViewItem, QRegion, QWidget, QBrush, QStyle,
                          QPen, QPushButton, QStyleOption, QMenu, QAction, QCursor,
-                         QTreeView, QLineEdit, QButtonGroup, QColor)
+                         QTreeView, QLineEdit, QButtonGroup, QColor, QTextCursor,
+                         QTextBlockFormat)
 
 from PyQt4.QtCore import (QPoint, QSize, QModelIndex, Qt, QObject, SIGNAL, QVariant,
-                          QAbstractItemModel, QRect, QRectF, QPointF)
+                          QAbstractItemModel, QRect, QRectF, QPointF, QStringList)
 
 signal_connect = QObject.connect
 
@@ -212,15 +213,15 @@ class AccountInputDialog(QDialog):
         self.widgets = widgets
         widgets.account.setModel(model)
         self.model = model
-        
         self.recent_list_write = []
         self.config = yobot_interfaces.component_registry.get_component("client-config")
         signal_connect(self.widgets.account, SIGNAL("currentIndexChanged(int)"), self.index_changed)
+        self._current_account = None
         
         def _add_to_recent():
             #only add if the user finds it worthwhile enough to act on the entry
             self.recent_list_write.append(self.widgets.target.currentText())
-            self.config.save()
+            self.update_recents()
         signal_connect(self, SIGNAL("accepted()"), _add_to_recent)
         
         signal_connect(self, SIGNAL("accepted()"), self.dialogDone)
@@ -235,16 +236,24 @@ class AccountInputDialog(QDialog):
         #sample implementation...
         if self.config:
             return self.config.get_account(acctobj).get("recent_contacts")
+    def update_recents(self):
+        log_err("override me")
+        return
+        #sample implementation:
+        if self.config and self._current_account:
+            self.config.get_account(autoadd=False)["recent_contacts"] = self.recent_list_write
         
     def index_changed(self, index):
         "subclasses should implement the 'recent_lookup' method"
         acctobj = self.model.index(self.widgets.account.currentIndex()).internalPointer()
+        #set the context
+        self._current_account = acctobj
         l = self.recent_lookup(acctobj)
         self.recent_list_write = l
         if l:
             self.widgets.target.clear()
-            self.widgets.target.addItems(l)
-        
+            for i in l:
+                self.widgets.target.addItem(i)
     
     def dialogDone(self):
         acct_obj = self.model.index(self.widgets.account.currentIndex()).internalPointer()
@@ -273,7 +282,18 @@ class SendJoinDialog(AccountInputDialog):
             return self.config.get_account(acctobj, autoadd=True).setdefault("recent_contacts", [])
         else:
             return self.config.get_account(acctobj, autoadd=True).setdefault("recent_chats", [])
-        
+    def update_recents(self):
+        if not self._current_account:
+            return
+        self.recent_list_write = map(str, self.recent_list_write)
+        conf_acct = self.config.get_account(self._current_account)
+        if self.type == IM:
+            conf_acct["recent_contacts"] = self.recent_list_write
+        else:
+            conf_acct["recent_chats"] = self.recent_list_write
+        log_err(self._current_account)
+        log_err(conf_acct)
+        self.config.save()
 
 class UserAddDialog(AccountInputDialog):
     def __init__(self, model, parent = None, type = None):
@@ -296,11 +316,63 @@ class DisconnectDialog(AccountInputDialog):
         self.fromServer = server
     def action(self, acct_obj, _null, _null2):
         acct_obj.disconnect(self.fromServer)
+        
+class _ChatText(object):
+    defaultFmt = QTextBlockFormat()
+    archFmt = QTextBlockFormat()
+    archFmt.setBackground(QBrush(QColor("#c0c0c0")))
+    def __init__(self, qtb):
+        self.qtb = qtb
+        self.append = self._initAppend
+    def _initAppend(self, txt, fmt=defaultFmt):
+        self.qtb.textCursor().mergeBlockFormat(fmt)
+        self.qtb.append(txt)
+        self.append = self._normalAppend
+    def _normalAppend(self, txt, fmt=defaultFmt):
+        c = self.qtb.textCursor()
+        c.movePosition(c.End)
+        c.insertBlock(fmt)
+        c.insertHtml(txt)
+    
+        
+class _IgnoreList(object):
+    def __init__(self, listwidget):
+        self.lw = listwidget
+        signal_connect(self.lw, SIGNAL("itemDoubleClicked(QListWidgetItem*)"), self._itemDoubleClicked)
+        self.users = {}
+    def __contains__(self, obj):
+        return obj in self.users
+    def _itemDoubleClicked(self, lwitem):
+        #remove user from ignore list:
+        if not lwitem:
+            return
+        username = str(lwitem.text())
+        self.remove(username)
+        
+    def add(self, username):
+        username = str(username)
+        if username in self.users:
+            return
+        lwitem = QListWidgetItem(QIcon(":/icons/res/16x16/actions/dialog-cancel.png"), username)
+        self.lw.addItem(lwitem)
+        self.users[username] = lwitem
+    def remove(self, username):
+        username = str(username)
+        if not username in self.users:
+            return
+        #remove the row assigned to this widget
+        row = self.lw.row(self.users.pop(username))
+        if row < 0:
+            log_err("row is < 0")
+            return
+        self.lw.takeItem(row)
+        log_debug("removed.. i think")
 
 class ChatWindow(QMainWindow):
     """This class is quite dumb, but it does contain client hooks to get and send
     messages"""
-    appearance_config = None
+    appearance_config = {}
+    use_relsize = False
     @classmethod
     def get_config(self):
         "set appearance configuration object. no need to set it each time"
@@ -313,7 +385,6 @@ class ChatWindow(QMainWindow):
     def __init__(self, client, parent=None, type=IM, acct_obj=None, target=None,
                  factory=None, initial_text="",):
         self.users = {} #dict containing the name of the user mapped to the model object..
-        self.ignore_list = set()
         
         if not target or not acct_obj:
             log_err( "must have target and account for chatwindow")
@@ -324,29 +395,43 @@ class ChatWindow(QMainWindow):
         self.account = acct_obj
         
         self.widgets = chatwindow_auto.Ui_w_chatwindow()
+        w = self.widgets
         
-        self.widgets.setupUi(self)
+        w.setupUi(self)
+        
+        self.ignore_list = _IgnoreList(w.ignorelist)
         self.setWindowTitle(target)
         #and some key press events..
-        self.widgets.input.keyPressEvent = self._inputKeyPressEvent
-        self.widgets.input.setHtml("")
-        self.widgets.convtext.setHtml(initial_text)
+        w.input.keyPressEvent = self._inputKeyPressEvent
+        w.input.mouseDoubleClickEvent = self._input_mouseDoubleClickEvent
+        w.input.setHtml("")
+        w.convtext.setHtml(initial_text)
         if type == CHAT:
-            self.widgets.userlist.show()
-            self.widgets.menuView.addAction(self.widgets.actionShow_User_List)
-            signal_connect(self.widgets.actionLeave, SIGNAL("activated()"), self.leaveRoom)
+            w.menuView.addAction(w.actionShow_User_List)
+            w.menuView.addAction(w.actionShow_Ignore_List)
+            w.menuActions.addAction(w.actionLeave)
+            w.actionShow_User_List.setChecked(True)
+            w.actionShow_Ignore_List.setChecked(True)
+            w.userlist.clear()
+            w.ignorelist.clear()
+            w.menuView.addAction(w.actionShow_User_List)
+            signal_connect(w.actionLeave, SIGNAL("activated()"), self.leaveRoom)
             
         elif type == IM:
-            self.widgets.userlist.hide()
-            signal_connect(self.widgets.actionShow_Backlog, SIGNAL("activated()"),
+            w.userlists.hide()
+            signal_connect(w.actionShow_Backlog, SIGNAL("activated()"),
                            lambda: self.account.getBacklog(self.target, self.defaultBacklogCount))
             #todo: use a dialog for this, perhaps...
             
         self.current_action_target = ""
-        self.widgets.userlist.clear()
+        w.userlist.clear()
         self.factory = factory
         self._init_input()
         self._init_menu()
+        self.get_config()
+        self.load_appearance_config()
+        self.chat_text = _ChatText(self.widgets.convtext)
+        
     def _init_input(self):
         #bold
         signal_connect(self.widgets.bold, SIGNAL("toggled(bool)"),
@@ -370,10 +455,6 @@ class ChatWindow(QMainWindow):
         #for updating the formatting buttons
         signal_connect(self.widgets.input, SIGNAL("currentCharFormatChanged(QTextCharFormat)"),
                        self._currentCharFormatChanged)
-        
-        #load formatting from configuration:
-        self.load_appearance_config()
-        
     def _choosecolor(self, set_color=None):
         def _onClicked(color):
             self.widgets.input.setTextColor(color)
@@ -385,9 +466,7 @@ class ChatWindow(QMainWindow):
         else:
             _onClicked(set_color)
 
-    
     def load_appearance_config(self):
-        self.get_config()
         w = self.widgets
         if self.appearance_config:
             #navigate around.. this could be tricky
@@ -396,51 +475,51 @@ class ChatWindow(QMainWindow):
                 f = QFont()
                 f.setFamily(c["font_family"])
                 w.font.setCurrentFont(f)
-            if c.get("font_size", None): w.fontsize.setValue(c["font_size"])
+            if c.get("font_size", None):
+                log_warn("setting font size to ", c["font_size"])
+                w.fontsize.setValue(c["font_size"])
+                w.input.setFontPointSize(c["font_size"])
             if c.get("font_color", None): self._choosecolor(set_color=QColor(c["font_color"]))
             if c.get("font_bold", False): w.bold.setChecked(True)
             if c.get("font_italic", False): w.italic.setChecked(True)
             if c.get("font_underline", False): w.underline.setChecked(True)
+            self.use_relsize = c.get("use_html_relsize", False)
             
             
     def _init_menu(self):
+        "Initializes context menu for usernames"
         if not self.type == CHAT:
             return
+        
         menu = QMenu()
-        self._action_newmsg = menu.addAction(
-            QIcon(":/icons/icons/message-new.png"), "Send IM")
-        self._action_ignore_tmp = menu.addAction(
-            QIcon(":/icons/res/16x16/actions/dialog-cancel.png"), "Ignore (from chat)")
-        self._action_ignore_perm = menu.addAction(
-            QIcon(":/icons/res/16x16/actions/dialog-cancel.png"), "Ignore (server)")
+        self._action_newmsg = menu.addAction(QIcon(":/icons/icons/message-new.png"), "Send IM")
+        self._action_ignore_tmp = menu.addAction(QIcon(":/icons/res/16x16/actions/dialog-cancel.png"), "Ignore (from chat)")
+        self._action_ignore_perm = menu.addAction(QIcon(":/icons/res/16x16/actions/dialog-cancel.png"), "Ignore (server)")
         
         if self.factory:
             signal_connect(self._action_newmsg, SIGNAL("activated()"),
-                           lambda: self.factory(target = self.current_action_target,
-                                                account = self.account, type = self.type))
+                lambda: self.factory(target = self.current_action_target, account = self.account, type = self.type))
             signal_connect(self.widgets.userlist, SIGNAL("itemDoubleClicked(QListWidgetItem*)"),
-                           lambda item: self.factory(
-                            target = item.text(), account = self.account, type = self.type))
-    
-        signal_connect(self._action_ignore_tmp, SIGNAL("activated()"),
-                       lambda: self.ignore_list.add(self.current_action_target))
+                lambda item: self.factory(target = item.text(), account = self.account, type = self.type))
+            
+        signal_connect(self._action_ignore_tmp, SIGNAL("activated()"), lambda: self.ignore_list.add(self.current_action_target))
         self.userActionMenu = menu
+        
         def _anchorClicked(link):
             link = str(link.toString())
             if link.startswith("YOBOT_INTERNAL"):
                 user = link.split("/", 1)[1]
-                if user:
-                    self.current_action_target = user
-                    self.userActionMenu.exec_(QCursor().pos())
-        signal_connect(self.widgets.convtext, SIGNAL("anchorClicked(QUrl)"),
-                       _anchorClicked)
+                self.current_action_target = user
+                self.userActionMenu.exec_(QCursor().pos())
+        signal_connect(self.widgets.convtext, SIGNAL("anchorClicked(QUrl)"), _anchorClicked)
         
         def _userlistContextMenu(point):
-            self.current_action_target = self.widgets.userlist.indexAt(point).data()
+            self.current_action_target = self.widgets.userlist.itemAt(point).text()
             self.userActionMenu.exec_(QCursor().pos())
+            
+        self.widgets.userlist.setContextMenuPolicy(Qt.CustomContextMenu)
         signal_connect(self.widgets.userlist, SIGNAL("customContextMenuRequested(QPoint)"),
                _userlistContextMenu)
-            
     
     def _currentCharFormatChanged(self,format):
         self.widgets.font.setCurrentFont(format.font())
@@ -449,12 +528,15 @@ class ChatWindow(QMainWindow):
         self.widgets.italic.setChecked(format.fontItalic())
         self.widgets.underline.setChecked(format.fontUnderline())
         self.widgets.fg_color.setStyleSheet("background-color: '%s'" % (format.foreground().color().name(),))
-                
+    
     def _inputKeyPressEvent(self, event):
         w = self.widgets
         key = event.key()
         modifiers = event.modifiers()
         if key== Qt.Key_Return:
+            if not str(self.widgets.input.toPlainText()):
+                log_debug("empty input")
+                return
             txt = self.widgets.input.toHtml()
             if not txt:
                 return
@@ -466,10 +548,12 @@ class ChatWindow(QMainWindow):
             return
         if key == Qt.Key_Backspace:
             cursor = self.widgets.input.textCursor()
-            if cursor.position() == 1:
+            if cursor.selectionStart() <=1:
                 #Don't erase formatting.. normally erasing the last character
                 #will also reset the format. This stores the current formatting,
                 #so that we can reset it after the character has been removed
+                #according to Qt documentation, selectionStart "Returns the end of
+                #the selection or position() if the cursor doesn't have a selection."
                 font = w.input.currentFont()
                 color = w.input.textColor()
                 cursor.deletePreviousChar()
@@ -488,22 +572,37 @@ class ChatWindow(QMainWindow):
         
         QTextEdit.keyPressEvent(self.widgets.input,event)    
     
+    def _input_mouseDoubleClickEvent(self, event):
+        cursor = self.widgets.input.textCursor()
+        if cursor.position() == 0:
+            log_debug("doing nothing")
+        else:
+            QTextEdit.mouseDoubleClickEvent(self.widgets.input, event)
+    
     def sendMsg(self, txt):
         chat = True if self.type == CHAT else False
         self.account.sendmsg(self.target, str(txt), chat=chat)
         
     def gotMsg(self, msg_obj):
         #get time..
+        w = self.widgets
         if msg_obj.who in self.ignore_list:
             return
-        msg_str = "<a href='YOBOT_INTERNAL/%s'>" % (msg_obj.who)
-        msg_str += "(%s) " % (msg_obj.timeFmt,) if self.widgets.actionTimestamps.isChecked() else ""
-        msg_str += "<font color='mediumblue'><b>%s</b></font></a>: " % (msg_obj.who,)
-        formatted = process_input(msg_obj.txt)
+        msg_str = ""
+        msg_str += "<a href='YOBOT_INTERNAL/%s'>" % (msg_obj.who)
+        msg_str += "(%s) " % (msg_obj.timeFmt,) if w.actionTimestamps.isChecked() else ""
+        msg_str += "<span style='color:mediumblue; font-weight:bold;'>%s</span></a>: " % (msg_obj.who,)
+        formatted = process_input(msg_obj.txt, self.use_relsize)
         formatted = insert_smileys(formatted, self.account.improto, ":smileys/smileys", 24, 24)
         log_debug(formatted)
         msg_str += formatted
-        self.widgets.convtext.append(msg_str)
+        if (msg_obj.yprotoflags & yobotproto.YOBOT_BACKLOG or
+            msg_obj.prplmsgflags & yobotproto.PURPLE_MESSAGE_DELAYED):
+            self.chat_text.append(msg_str, fmt=self.chat_text.archFmt)
+        else:
+            self.chat_text.append(msg_str)
+        sb = w.convtext.verticalScrollBar()
+        sb.setValue(sb.maximum())
     
     def userJoined(self, user):
         if self.users.get(user): return
@@ -518,7 +617,10 @@ class ChatWindow(QMainWindow):
         self.users.pop(u,"")
         
     def leaveRoom(self):
-        self.account.leaveRoom(self.target)    
+        self.account.leaveRoom(self.target)
+    def roomLeft(self, msg=None):
+        log_err("implement me!")
+        pass
 
 class StatusDialog(object):
     def __init__(self, status_mappings, accept_fn):
@@ -843,7 +945,10 @@ class YobotGui(object):
         #fetch offline messages
         acct_obj.fetchBuddies()
         acct_obj.getOfflines()
-        
+    
+    def connectProgress(self, acct, msg):
+        self.mw_widgets.statusbar.showMessage(acct.name + ": " + msg)
+    
     def gotMessage(self, acct_obj, msg_obj):
         #log_debug(msg_obj)
         #FIXME: hack..
@@ -862,21 +967,17 @@ class YobotGui(object):
         type = CHAT if msg_obj.isChat else IM
         self._openChat(acct_obj, name, type)
         self.chats[acct_obj, name].gotMsg(msg_obj)
-        
     def chatUserJoined(self, acct_obj, room, user):
         c = self.chats.get((acct_obj,room))
         if not c: return
         c.userJoined(user)
-
     def chatUserLeft(self, acct_obj, room, user):
         c = self.chats.get([acct_obj, room])
         if not c: return
         c.userLeft(user)
-    
     def gotRequest(self, request_obj):
         log_err(str(request_obj))
         self.notifications.addItem(request_obj)
-    
     def delRequest(self, acct, refid):
         self.notifications.delItem(acct, refid)
     def connectionFailed(self, acct, txt):
@@ -886,6 +987,10 @@ class YobotGui(object):
         self.notifications.addItem(m)
     def roomJoined(self, acct, room):
         acct.fetchRoomUsers(room)
+    def roomLeft(self, acct, room):
+        log_err("implement this!!!")
+        #preferrably, this function should print a message in the chat window,
+        #if open, notifying the user about the leave
     
     accountConnectionFailed = connectionFailed        
     genericNotice = gotRequest
