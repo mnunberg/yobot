80ffaefce1872223fb44aad94ec9c8b7ed083284
Mark Nunberg <mnunberg@haskalah.org>
2010-10-19 01:19:54 -0700

    Meh.. nothing much

--- launch.bat
+++ launch.bat
@@ -3,8 +3,8 @@
 @set sleep=%yobotroot%\tools\_sleep.exe
 @set PATH=%PATH%;%pidginroot%;%pidginroot%\Plugins;%yobotroot%;%yobotroot%\contrib
 @set LOGPATH=Z:\
-start "Purple" cmd /c "%yobotroot%\yobot.exe -d 1 > %LOGPATH%\purple.log% 2>&1"
+start "Purple" cmd /c "%yobotroot%\yobot.exe --debug=0"
 rem %sleep% 1000
 start "Yobot Agent" cmd /c "C:\python26\python.exe %yobotroot%\py\yobotnet.py"
 rem %sleep% 1000
-start "QYobot" cmd /c "C:\python26\python.exe %yobotroot%\py\client.py -p triviabot -p gui_main"
+start "QYobot" cmd /c "C:\python26\python.exe %yobotroot%\py\client.py -p triviabot -p gui_main >> Z:\client.log"
--- linecount.sh
+++ linecount.sh
@@ -18,7 +18,7 @@ yobot_win32.c,\
 yobot_win32.h} \
 py/{account,client_config,client_support,debuglog,msglogger,startyobot,\
 yobotclass,yobot_interfaces,yobotnet,yobotops,yobot_plugins}.py \
-py/gui/{config_dialog,gui_util,html_fmt,qyobot,yahoo_captcha,chatwindow}.py \
-py/triviabot/{gui_new,triviabot}.py)
+py/gui/{config_dialog,gui_util,html_fmt,qyobot,yahoo_captcha,chatwindow,tabwindows}.py \
+py/triviabot/{gui_new,triviabot,triviadb}.py)
 wc -l $file_list | sort -n
 sloccount $file_list
